06b32879d2f88ea261d99df72faefd51b4e9fa5f
jpucic00 <jpucic00@gmail.com>
2026-03-02 17:14:13 +0100

    Remove deprecated gui/ (CRA) and server/ (Express) directories
    Fully replaced by the Next.js frontend/ app.

--- gui/src/constants/menuItems.jsx
+++
@@ -1,8 +0,0 @@
-export const navigationMenuItems = [
-  { name: "Home", link: "/" },
-  { name: "Details", link: "/details" },
-  { name: "Gallery", link: "/gallery" },
-  { name: "Calendar", link: "/calendar" },
-  { name: "Contact", link: "/contact" },
-  { name: "Log in", link: "/login" },
-];
